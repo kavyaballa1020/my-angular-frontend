10b3e2611ac11389ce26d8c7ca838e9500bd01ee
Balla Kavya <ballakavya594@gmail.com>
2024-10-31 15:56:43 +0530

    "Updated CSS and HTML for Ponds and Sensors components, including layout, styling, and icon changes."

--- src/app/components/ponds/ponds.component.ts
+++ src/app/components/ponds/ponds.component.ts
@@ -13,17 +13,30 @@ import { Router, RouterModule } from '@angular/router';
 export class PondsComponent {
   ponds = [
     {
-      name: 'Pond 1',
-      image: 'https://www.agriculture.com/thmb/F470pJd4uVVhiJuZDJ1gOTuUE8g=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/100008958_preview-efb5c8518bbd4f9689fe61d785fb4a4b.jpg'
+      name: 'Blue Lagoon',
+      image: 'https://www.agriculture.com/thmb/F470pJd4uVVhiJuZDJ1gOTuUE8g=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/100008958_preview-efb5c8518bbd4f9689fe61d785fb4a4b.jpg',
+      description: 'Known for its clear blue water and vibrant marine life.',
+            address: '123 Lake Road, Springfield',
+
+
     },
     {
-      name: 'Pond 2',
-      image: 'https://pondandgarden.com.au/wp-content/uploads/2022/12/aquascape-pond-ecosystem-1024x1024.jpeg.webp'
+      name: 'Silver Pond',
+            address: '456 Lagoon Ave, Riverdale',
+
+      image: 'https://pondandgarden.com.au/wp-content/uploads/2022/12/aquascape-pond-ecosystem-1024x1024.jpeg.webp',
+            description: 'A quiet pond nestled in a serene environment with scenic views.'
+
     },
     {
-      name: 'Pond 3',
-      image: 'assets/image3.jpg' // Use your local image path here
-    }
+      name: 'Green Lake',
+            address: '789 Pond Street, Oceanview',
+
+      image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTlkv5DggDfh7BHbmBTTVAJSevl3x2MKY7Imw&s' ,
+            description: 'A beautiful lake surrounded by greenery and perfect for fishing.'
+
+    },
+
   ];
 
   viewDetails(pond: any) {
